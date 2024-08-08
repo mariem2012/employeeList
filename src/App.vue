d32fb4dd4686b5ccfb871a7984e1aba5553ded87
mariem2012 <mariemdianifaba00@gmail.com>
2024-08-08 15:43:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Liste des employés</h1>
+    <EmployeeList />
+  </div>
+</template>
+
+<script>
+import EmployeeList from './components/EmployeeList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EmployeeList
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #20C997;
+}
+#app {
+  display: flex;
+  align-content: center;
+  justify-content: center; 
+  flex-direction: column;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,51 @@
+<template>
+  <ol class="employee-list">
+    <li 
+      v-for="(employee, index) in employees" 
+      :key="index" 
+      class="employee-item"
+    >
+      {{ employee.name }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'EmployeeList',
+  data() {
+    return {
+      employees: [
+        { name: 'Assa Baradji' },
+        { name: 'Thillo Sem' },
+        { name: 'Aichetou Sy' },
+        { name: 'Fatima Cissé' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.employee-list {
+  list-style-type: decimal;
+  padding: 0;
+  margin: 20px;
+  width: 300px;
+ 
+}
+
+.employee-item {
+  background-color: #f9f9f9;
+  margin: 10px 0;
+  padding: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.employee-item:hover {
+  background-color:#2d4768  ;
+  cursor: pointer;
+  color: #f9f9f9;
+}
+</style>
